ebb2f734cb3949286515afd4453976bf152e9fbe
Daniil Shumko <7705571+daniil-shumko@users.noreply.github.com>
2022-04-07 13:55:47 +0100

    Removed no longer needed create_ipa.sh file

--- .github/scripts/create_ipa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-xcodebuild -archivePath $PWD/build/Runner.xcarchive \
--exportOptionsPlist $PWD/ExportOptions.plist \
--exportPath $PWD/build \
--exportArchive | xcpretty
